36c646ca5977c44de88459ebafeb5746f506b195
Vidyaabharathi Vasudevan <vvasude6@asu.edu>
2017-07-09 11:54:59 -0700

    Modifying Powerset

--- Powerset.java
+++ Powerset.java
@@ -1,13 +1,11 @@
-
-
- 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
  
 public class Powerset {
  
- public static void main(String[] args) {  
+ public static void main(String[] args) 
+ {  
   System.out.println("\n"+createSubsetUsingTree("ABCD"));
  } 
  
